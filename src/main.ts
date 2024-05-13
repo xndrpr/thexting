fcd0599119129390ae637b9e37e4d2356407beb3
uaquax <uaquax@tutanota.com>
2024-05-14 02:13:04 +0800

    cors

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
+  app.enableCors({ origin: '*' });
+  // app.enableCors({ origin: 'http://localhost:3000' });
 
   await app.listen(6100);
 }
