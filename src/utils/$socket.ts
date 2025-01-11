9d3e3ff9b696a9c89b7aa69f98ff1bd8e02e11a3
xndrpr <velaton618@gmail.com>
2025-01-11 10:17:55 +0800

    Remove transport

--- src/utils/$socket.ts
+++ src/utils/$socket.ts
@@ -1,7 +1,5 @@
 import { createContext } from "react";
 import { io } from "socket.io-client";
 
-export const $socket = io(process.env.REACT_APP_SOCKET_URL, {
-  transports: ["websocket"],
-});
+export const $socket = io(process.env.REACT_APP_SOCKET_URL);
 export const SocketContext = createContext($socket);
