ece76ed7a6d161701a0259b4d4ff8fd3098a27a2
xndrpr <velaton618@gmail.com>
2025-01-13 16:57:18 +0800

    done

--- src/shared/$socket.ts
+++ src/shared/$socket.ts
@@ -1,7 +1,7 @@
 import { createContext } from "react";
 import { io } from "socket.io-client";
 
-export const $socket = io("/", {
+export const $socket = io("wss://xndrpr.ddns.net", {
   transports: ["websocket"],
   withCredentials: true,
   secure: true,
